9e2eb69bc4bda725eb78c5c08f231598b3241dd5
Andre Emidio <andresjc2008@gmail.com>
2017-10-29 13:57:45 -0200

    Criação de novos arquivos de classificação de imagens, com novos algoritmos com arquivos de Yale e teste de aprendizado e acerto.

--- Reconhecimento Facial/testeClassificadorYale.py
+++ Reconhecimento Facial/testeClassificadorYale.py
@@ -3,19 +3,19 @@ import os
 import numpy as np
 from PIL import Image
 
-detectorFace = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+detectorFace = cv2.CascadeClassifier("cascades\haarcascade_frontalface_default.xml")
 #reconhecedor = cv2.face.EigenFaceRecognizer_create()
 #reconhecedor.read("ArquivosTreinamentos\classificadorEigenYale.yml")
 #reconhecedor = cv2.face.FisherFaceRecognizer_create()
 #reconhecedor.read("ArquivosTreinamentos\classificadorFisherYale.yml")
 reconhecedor = cv2.face.LBPHFaceRecognizer_create()
-reconhecedor.read("ArquivosTreinamentos\cclassificadorLBPHYale.yml")
+reconhecedor.read("ArquivosTreinamentos\classificadorLBPHYale.yml")
 
 totalAcertos = 0
 percentualAcerto = 0.0
 totalConfianca = 0.0
 
-caminhos = [os.path.join('yalefaces\teste', f) for f in os.listdir('yalefaces\teste')]
+caminhos = [os.path.join('yalefaces/teste', f) for f in os.listdir('yalefaces/teste')]
 for caminhoImagem in caminhos:
     imagemFace = Image.open(caminhoImagem).convert('L')
     imagemFaceNP = np.array(imagemFace, 'uint8')
@@ -23,15 +23,19 @@ for caminhoImagem in caminhos:
     for (x, y, l, a) in facesDetectadas:
         idprevisto, confianca = reconhecedor.predict(imagemFaceNP)
         idatual = int(os.path.split(caminhoImagem)[1].split(".")[0].replace("subject", ""))
-        print(str(idatual) + " foi classificado como " + str(idprevisto) + " - " + str(confianca))
+        print(str(idatual) + " foi classificado como " + str(idprevisto) + " com confiança de  " + str(confianca))
         if idprevisto == idatual:
             totalAcertos += 1
             totalConfianca += confianca
         #cv2.rectangle(imagemFaceNP, (x, y), (x + l, y + a), (0, 0, 255), 2)
         #cv2.imshow("Face", imagemFaceNP)
         #cv2.waitKey(1000)
+
+
 percentualAcerto = (totalAcertos / 30) * 100
 totalConfianca = totalConfianca / totalAcertos
 print("Percentual de acerto: " + str(percentualAcerto))
 print("Total confiança: " + str(totalConfianca))
+
+
 
